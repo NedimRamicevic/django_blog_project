4d6ab5179a871e7cf061123ace4e19aa16ffeef4
Nedim <mr.ramicevic@gmail.com>
2022-07-26 11:57:51 +0300

    register user

--- Users/views.py
+++ Users/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             user = form.cleaned_data.get('username')
             messages.success(request, 'Account created for ' + user)
             return redirect('blog-home')
-        else:
-            return redirect('register')
+        
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
         return render(request, 'users/register.html', {'form': form})
