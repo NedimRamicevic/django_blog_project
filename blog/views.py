06acfee96019342044809b3241e776be2565ab63
Nedim <mr.ramicevic@gmail.com>
2022-07-28 12:26:14 +0300

    post update delete

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,7 @@ from .models import Post
 
 class PostListView(ListView):
     model = Post
-    template_name = 'home.html'
+    template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
     paginate_by = 5
